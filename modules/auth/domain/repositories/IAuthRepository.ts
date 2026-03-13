2c4a3b4af5fb9919d8b1481410b6c668e61a7457
izaisaguilar <izais.aguilar@gmail.com>
2026-03-13 15:10:35 -0600

    Auth

--- modules/auth/domain/repositories/IAuthRepository.ts
+++ modules/auth/domain/repositories/IAuthRepository.ts
@@ -1,15 +1,6 @@
 import { User } from "../entities/User";
 
-// Contrato que deben cumplir todos los repositorios de autenticación
 export interface IAuthRepository {
-  register(
-    fullName: string,
-    email: string,
-    password: string
-  ): Promise<User>;
-
-  login(
-    email: string,
-    password: string
-  ): Promise<User>;
+  register(fullName: string, email: string, password: string): Promise<User>;
+  login(email: string, password: string): Promise<User>;
 }
